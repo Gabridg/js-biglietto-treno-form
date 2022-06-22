48cac7350d5b708eb05424f3b7fee4434deec8c5
Gabridg <gabri99.del@gmail.com>
2022-06-22 17:52:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+/*
+
+Il programma dovrà mostrare un form da compilare con cui chiedere all'utente il numero di
+ chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo
+queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina
+(formattato con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+
+
+Bonus:
+Aggiungere stile in modo da avere una bella pagina per la nostra compagnia di treni.
+Allego un'immagine di riferimento per chi non ha fantasia. :occhiolino:
+Potete usare quel che vi pare (Bootstrap, bool12 , CSS plain)
+Se volete potete anche spingervi al responsive ma l'importante è che facciate
+funzionare il form e stampare correttamente i dati del biglietto!
+
+*/
+
+/*
+
+1- recuperare gli input/bottoni
+2- calcolare la distanza in km (*0.21)
+3- calcolare il prezzo in base agli sconti
+4-
+5-
+*/
+
+// 1 -
+const proceedButton = document.getElementById('proceed');
+const backButton = document.getElementById('back')
+console.log('button proceed: ', proceedButton);
+console.log('button back', backButton);
+
+const inputName = document.getElementById('name');
+const inputKm = document.getElementById('distance');
+const inputAge = document.getElementById('age-selection');
+console.log('nome e cognome: ', inputName.value);
+console.log('distanza da percorrere: ', inputKm.value);
+console.log('età utente: ', inputAge.value);
+
+const ticket = document.getElementById('ticket');
+
+// 2 -
+
+const price = inputKm.value * 0.21;
+console.log('Prezzo senza  sconto €: ', price);
+
+let hasDiscount = false
+
+if (document.getElementById('age-selection').options.item(1)) {
+    hasDiscount = true
+    let finalPrice = price - (price * 0.20);
+    console.log('Prezzo scontato €: ', finalPrice);
+}
+
+if (document.getElementById('age-selection').options.item(2)) {
+    hasDiscount = true
+    let finalPrice = price - (price * 0.4)
+    console.log('prezzo scontato €: ' + finalPrice);
+}
+
+
+
+
+
+
+
+
+
+
+
+proceedButton.addEventListener('click', function () {
+
+
+
+
+    const userName = inputName.value;
+    const userKm = inputKm.value;
+    const userAge = inputAge.value;
+    ticket.innerText = userName + ' ' + userKm + '(km) ' + userAge + ' ' + price + '€';
+
+})
+
